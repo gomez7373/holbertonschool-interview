f0520e277d2e96e36716b9f3275ef514659a6b2a
gomez7373 <sgc.holberton@gmail.com>
2025-01-01 21:30:41 -0400

    tasks

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -6,23 +6,30 @@
 
 
 def canUnlockAll(boxes):
-    """ Determines if all the boxes can be unlocked """
-    if boxes:
-        keys = [0]
-        keysNeeded = [i[0] for i in enumerate(boxes)]
-
-        # will Collect the keys from all the un-lockable boxes that beginnings with 0
-        collectKeys(boxes, keys)
-
-        # Will return true or false if keys present match key needed or not 
-        return sorted(keys) == keysNeeded
-    else:
+    """
+    Determines if all the boxes can be unlocked.
+    
+    Args:
+        boxes (list of list of int): A list where each element is a list of keys.
+    
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise.
+    """
+    if not boxes:
         return True
 
+    n = len(boxes)
+    unlocked = [False] * n  # Keep track of unlocked boxes
+    unlocked[0] = True  # The first box is unlocked by default
+    keys = [0]  # Start with the key for the first box
 
-def collectKeys(boxes, keys, key=0):
-    # Will recursively collect all the unique keys from all the boxes 
-    for newKey in boxes[key]:
-        if newKey not in keys and newKey < len(boxes):
-            keys.append(newKey)
-            collectKeys(boxes, keys, newKey)
+    while keys:
+        current_key = keys.pop()
+
+        for new_key in boxes[current_key]:
+            if 0 <= new_key < n and not unlocked[new_key]:
+                unlocked[new_key] = True  # Mark the box as unlocked
+                keys.append(new_key)  # Add the key to explore further
+
+    # Check if all boxes are unlocked
+    return all(unlocked)
