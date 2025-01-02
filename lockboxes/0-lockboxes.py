c64eaeeaf776c894ae3e34da2b738c003f01f48b
gomez7373 <sgc.holberton@gmail.com>
2025-01-01 21:21:07 -0400

    tasks

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -5,24 +5,39 @@
 """
 
 
-def canUnlockAll(boxes):
-    """ Determines if all the boxes can be unlocked """
-    if boxes:
-        keys = [0]
-        keysNeeded = [i[0] for i in enumerate(boxes)]
-
-        # will Collect the keys from all the un-lockable boxes that beginnings with 0
-        collectKeys(boxes, keys)
-
-        # Will return true or false if keys present match key needed or not 
-        return sorted(keys) == keysNeeded
-    else:
+def can_unlock_all(boxes):
+    """
+    Determines if all the boxes can be unlocked.
+    
+    Args:
+        boxes (list of list of int): A list where each element is a list of keys.
+    
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise.
+    """
+    if not boxes:
         return True
 
+    keys = [0]
+    keys_needed = list(range(len(boxes)))
 
-def collectKeys(boxes, keys, key=0):
-    # Will recursively collect all the unique keys from all the boxes 
-    for newKey in boxes[key]:
-        if newKey not in keys and newKey < len(boxes):
-            keys.append(newKey)
-            collectKeys(boxes, keys, newKey)
+    # Collect keys from all unlockable boxes starting with box 0
+    collect_keys(boxes, keys)
+
+    # Check if all required keys are collected
+    return sorted(keys) == keys_needed
+
+
+def collect_keys(boxes, keys, current_key=0):
+    """
+    Recursively collect all unique keys from the boxes.
+    
+    Args:
+        boxes (list of list of int): A list where each element is a list of keys.
+        keys (list of int): List of collected keys.
+        current_key (int): Current box being unlocked.
+    """
+    for new_key in boxes[current_key]:
+        if new_key not in keys and new_key < len(boxes):
+            keys.append(new_key)
+            collect_keys(boxes, keys, new_key)
