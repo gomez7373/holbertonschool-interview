4589b2219ce15b401a2677e493e6d12fb33260c0
gomez7373 <sgc.holberton@gmail.com>
2025-04-24 08:15:54 -0400

    task

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Minimum Operations """
+
+
+def minOperations(n):
+    if n < 2:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
+
